e6d1a70f21be65e40a48017186cf33ab85c383ed
tomika <tamas10.rez@gmail.com>
2025-04-27 14:28:14 +0200

    added cards

--- JS/main.js
+++ JS/main.js
@@ -7,16 +7,27 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
     const elements = document.querySelectorAll(".scroll");
-
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
-            if (entry.isIntersecting && entry.intersectionRatio >= 0.7) {
+            if (entry.isIntersecting && entry.intersectionRatio >= 0.6) {
                 entry.target.classList.add("show");
             }
         });
-    }, { threshold: 0.7 });
+    }, { threshold: 0.6 });
 
     elements.forEach(element => observer.observe(element));
+
+
+    const dropElements = document.querySelectorAll(".dropScroll");
+    const observer2 = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting && entry.intersectionRatio >= 1) {
+                entry.target.classList.add("dropShow");
+            }
+        });
+    }, { threshold: 1 });
+
+    dropElements.forEach(dropElement => observer2.observe(dropElement));
 });
 
 let section2 = document.querySelector(".section2");
@@ -29,4 +40,13 @@ section2.addEventListener("mousemove", function(e) {
     section2.style.setProperty('--y', y + 'px');
 })
 
-console.log(section2.parentElement)
+let cards = document.querySelectorAll('.cards');
+cards.forEach(card => {
+    card.onmousemove = function(e) {
+        let cardX = e.pageX - card.offsetLeft;
+        let cardY = e.pageY - card.offsetTop;
+
+        card.style.setProperty('--cardX', cardX + 'px');
+        card.style.setProperty('--cardY', cardY + 'px');
+    }
+})
